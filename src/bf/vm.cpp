3558ddf583f711318ae556a344f7bf3172171463
AsuMagic <sdelang@sdelang.fr>
2017-12-24 14:13:15 +0100

    Small VM cleanup

--- src/bf/vm.cpp
+++ src/bf/vm.cpp
@@ -6,25 +6,20 @@
 namespace bf
 {
 	// Performance-critical threaded interpreter
-	void Brainfuck::interprete(const size_t memory_size) noexcept
+	void Brainfuck::interprete(size_t memory_size) noexcept
 	{
-		std::vector<uint8_t> memory(memory_size);
+		using Cell = int8_t;
+		std::vector<Cell> memory(memory_size);
 
-		std::array<void*, Opcode::bfTOTAL> labels =
-		{{
-			&&lAdd, &&lShift,
-			&&lCOut, &&lCIn,
-			&&lJZ, &&lJNZ,
-			&&lSet,
-			&&lSUZ,
-			&&lEnd,
+		constexpr std::array<void*, Opcode::bfTOTAL> labels {{
+			&&lAdd, &&lShift, &&lCOut, &&lCIn, &&lJZ, &&lJNZ, &&lSet, &&lSUZ, &&lEnd
 		}};
 
 		// Compute the goto labels
 		for (auto &ins : program)
 			ins.handler = labels[ins.opcode];
 
-		uint8_t* sp = memory.data();
+		Cell *sp = memory.data();
 		const Instruction *pc = program.data();
 				
 		dispatch_noinc();
@@ -32,11 +27,11 @@ namespace bf
 		lAdd:  *sp += pc->argument; dispatch();
 		lShift: sp += pc->argument; dispatch();
 		
-		lCOut: std::cout << *sp << std::flush; dispatch();
-		lCIn:  std::cin  >> *sp; dispatch();
+		lCOut: std::cout << static_cast<char>(*sp) << std::flush; dispatch();
+		lCIn:  char c; std::cin >> c; *sp = c; dispatch();
 		
-		lJZ:  if (*sp == 0) { pc = &program[pc->argument]; dispatch_noinc(); } dispatch();
-		lJNZ: if (*sp != 0) { pc = &program[pc->argument]; dispatch_noinc(); } dispatch();
+		lJZ:  if (*sp == 0) { pc = program.data() + pc->argument; dispatch_noinc(); } dispatch();
+		lJNZ: if (*sp != 0) { pc = program.data() + pc->argument; dispatch_noinc(); } dispatch();
 		
 		lSet: *sp = pc->argument; dispatch();
 
